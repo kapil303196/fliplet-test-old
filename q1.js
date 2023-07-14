e5bb47e83d3381256fde8d28f17b22295e1a074c
Kapil Upadhyay <kapil303196@gmail.com>
2023-07-14 13:46:42 +0530

    initial commit

---
+++ q1.js
@@ -0,0 +1,91 @@
+const _ = require('lodash');
+
+const sampleData = {
+  apps: [
+    { id: 1, title: 'Lorem', published: true, userId: 123 },
+    { id: 2, title: 'Ipsum', published: false, userId: 123 },
+    { id: 3, title: 'Dolor', published: true, userId: 456 },
+    { id: 4, title: 'Sit', published: true, userId: 789 },
+    { id: 5, title: 'Amet', published: false, userId: 123 },
+    { id: 6, title: 'Et', published: true, userId: 123 }
+  ],
+  organizations: [
+  	{ id: 1, name: 'Google', suspended: true, userId: 123 },
+    { id: 2, name: 'Apple', suspended: false, userId: 456 },
+    { id: 3, name: 'Fliplet', suspended: false, userId: 123 }
+  ]
+}
+
+class User {
+  constructor(config) {
+    this.id = config.id;
+    this.tableName = '';
+    this.attrs = [];
+    this.conditions = {};
+    this.sort = [];
+  }
+  
+  select(tableName) {
+    this.tableName = tableName;
+    return this;
+  }
+  
+  attributes(attrs) {
+    this.attrs = attrs;
+    return this;
+  }
+  
+  where(conditions) {
+    this.conditions = conditions;
+    return this;
+  }
+  
+  order(sort) {
+    this.sort = sort;
+    return this;
+  }
+  
+  findAll() {
+    return new Promise((resolve) => {
+      let results = _.filter(sampleData[this.tableName], { userId: this.id, ...this.conditions });
+      results = _.map(results, item => _.pick(item, this.attrs));
+      if (this.sort.length > 0) {
+        results = _.orderBy(results, this.sort);
+      }
+      resolve(results);
+    });
+  }
+  
+  findOne() {
+    return new Promise((resolve) => {
+      let result = _.find(sampleData[this.tableName], { userId: this.id, ...this.conditions });
+      if (result) {
+        result = _.pick(result, this.attrs);
+      }
+      resolve(result);
+    });
+  }
+}
+
+// Testing Code
+
+let user = new User({ id: 123 });
+
+user
+	.select('apps')
+  	.attributes(['id', 'title'])
+  	.where({ published: true })
+  	.order(['title'])
+  	.findAll()
+  	.then((apps) => {
+    	console.log(apps);
+  	});
+
+user
+	.select('organizations')
+  	.attributes(['id', 'name'])
+  	.where({ suspended: false })
+  	.findOne()
+  	.then((organization) => {
+    	console.log(organization);
+  	});
---
+++ q3.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+const _ = require('lodash');
+
+async function parse(inputArray) {
+  let data;
+
+  try {
+    const response = await axios.get('https://api.fliplet.com/v1/widgets/assets');
+    data = response.data;
+  } catch (error) {
+    console.error(`HTTP error! status: ${error}`);
+  }
+
+  let assets = [];
+  let processedPackages = new Set(); // Keep track of the packages we've already processed
+
+  // Iterating in the order of the input array
+  for (let item of inputArray) {
+    if (processedPackages.has(item)) {
+      continue;
+    }
+
+    let packageData = data.assets[item];
+    if (packageData) {
+      let packageAssets = _.get(packageData, ['versions', packageData.latestVersion], []);
+      
+      if (_.isEmpty(packageAssets)) {
+        // If the package has no assets of its own, look into its dependencies
+        const dependencies = _.get(packageData, 'dependencies', []);
+        
+        // Reorder dependencies to prioritize ones not in the input array
+        const orderedDependencies = _.concat(_.difference(dependencies, inputArray), _.intersection(dependencies, inputArray));
+        
+        const dependencyAssets = _.flatMap(orderedDependencies, (dep) => {
+          if (processedPackages.has(dep)) {
+            return [];
+          }
+          processedPackages.add(dep);
+          return _.get(data, ['assets', dep, 'versions', data.assets[dep].latestVersion], []);
+        });
+        
+        assets = _.concat(assets, dependencyAssets);
+      } else {
+        // If the package has assets, add them
+        assets = _.concat(assets, packageAssets);
+      }
+    }
+    processedPackages.add(item);
+  }
+
+  return assets;
+}
+
+parse(['bootstrap', 'fliplet-core', 'moment', 'jquery']).then((assets) => {
+  console.log('The list is', assets);
+}).catch(error => console.log(error));
